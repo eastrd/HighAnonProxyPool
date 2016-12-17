58d504c96baffb1c58814e9ce161a69d23da9fb2
eastrd <yjia285@student.monash.edu>
2016-12-17 16:26:37 +1100

    重命名

--- Modules/CUI.py
+++ Modules/CUI.py
@@ -5,3 +5,16 @@
 import scraper_manager
 import proxy
 import db
+from time import sleep
+import threading
+
+#启动爬虫与代理验证模块
+print("[!] 启动爬虫模块...")
+threading.Thread(target=scraper_manager.start, name='Scraper Manager').start()
+print("[!] 启动验证模块...")
+threading.Thread(target=proxy.start, name='Proxy Manager').start()
+
+#CUI界面循环
+while True:
+	print("目前数据库中有%s个代理"%len(db.Database().fetch_all()))
+	sleep(1)
