092868ce113424081db366bf4a046717403141c7
eastrd <yjia285@student.monash.edu>
2016-12-17 21:44:31 +1100

    小更新

--- Modules/proxy.py
+++ Modules/proxy.py
@@ -52,7 +52,7 @@ class Proxy:
 		for ProxyRecord_tuples in DirtyProxyList:
 			#如果同时存在多于100个线程，则等待10秒再开新线程
 			while threading.activeCount() > 100:
-				pass
+				sleep(1)
 			ProxyCheckerThread(ProxyRecord_tuples).start()
 		#print("[!] 当前代理循环验证任务线程发布完毕\n\n")
 
